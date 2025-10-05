a8eca474b7b638fda7edae96c6c9577929499b11
MilanaAleksandrova <aleksandrova.milana.g@gmail.com>
2025-10-05 18:01:04 +0300

    Fix the mathematical logic on the backend

--- src/main/java/dev/lab1/Main.java
+++ src/main/java/dev/lab1/Main.java
@@ -115,11 +115,11 @@ public class Main {
         if (x >= 0 && y >= 0) {
             return x <= r && y <= r;
         }
-        if (x <= 0 && y >= 0) {
-            return y <= 2 * x + r;
-        }
         if (x <= 0 && y <= 0) {
             return (x * x + y * y) <= (r * r);
+        }
+        if (x <= 0 && y >= 0) {
+            return y <= 2 * x + r;
         }
         return false;
     }
