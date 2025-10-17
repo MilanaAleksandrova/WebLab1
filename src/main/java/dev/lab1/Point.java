a9eec3c7e267f36add138797384345cbd6d1847c
MilanaAleksandrova <aleksandrova.milana.g@gmail.com>
2025-10-17 12:00:30 +0300

    Use the record to simplify the class structure

--- src/main/java/dev/lab1/Point.java
+++ src/main/java/dev/lab1/Point.java
@@ -2,18 +2,5 @@ package dev.lab1;
 
 import java.math.BigDecimal;
 
-class Point {
-    BigDecimal x, y, r;
-    String currentTime;
-    long executionTime;
-    boolean hit;
+public record Point(BigDecimal x, BigDecimal y, BigDecimal r, String currentTime, long executionTime, boolean isHit) {}
 
-    Point(BigDecimal x, BigDecimal y, BigDecimal r, String currentTime, long executionTime, boolean hit) {
-        this.x = x;
-        this.y = y;
-        this.r = r;
-        this.currentTime = currentTime;
-        this.executionTime = executionTime;
-        this.hit = hit;
-    }
-}
